29b38244f7ea87744f574503281e1d9ddeb72c68
Rafael Coelho <rcoelho_de_oliveira@hotmail.com>
2025-04-28 22:10:43 -0300

    Adicionado prova

---
+++ DML.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE Cinema;
+use Cinema;
+CREATE TABLE Filme (
+    id_filme INT AUTO_INCREMENT PRIMARY KEY,
+    titulo varchar(255) NOT NULL,
+    duraca_minutos INT NOT NULL,
+    genero varchar(50)
+);
+
+CREATE TABLE Sessao (
+    id_sessao INT AUTO_INCREMENT PRIMARY KEY,
+    id_filme int NOT NULL,
+    data_sessao DATE NOT NULL,
+    horario time,
+    capacidade_maxima int NOT NULL,
+    FOREIGN KEY (id_filme) REFERENCES Filme(id_filme) ON DELETE CASCADE
+);
+
+CREATE TABLE Ingresso (
+    id_ingresso INT AUTO_INCREMENT PRIMARY KEY,
+    id_sessao int NOT NULL,
+    nome_client varchar(100) NOT NULL,
+    valor_pago decimal(6,2) NOT NULL,
+    FOREIGN KEY (id_sessao) REFERENCES Sessao(id_sessao) ON DELETE CASCADE
+);
---
+++ INSERT.sql
@@ -0,0 +1,13 @@
+
+
+INSERT INTO filme
+    (titulo, duracao_minutos,genero)
+VALUES 
+    ('Pirates of silicon Valley', 95 , 'Drama'),
+    ('The social Network', 120 , 'Drama'),
+    ('Jobs',128,'Drama'),
+    ('The imitation Game',113,'Suspense'),
+    ('Snowden',134,'Suspense');
+    
+Select * from filme;  
+    
